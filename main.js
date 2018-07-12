f3e59d764206c6d68d002bb7cc34e1721fd3bf98
dzmitryshaliaheika <shelegeykodmitriy@gmail.com>
2018-07-13 00:21:34 +0300

    change js and css, optimization video and pictures

--- main.js
+++ main.js
@@ -3,12 +3,17 @@
   /*varibles*/
   var changePosition,
     getRandom,
+    isTouchDevice,
+    autoCloseVideo,
+    addAnimateText,
+    removeAnimateText,
     video = $('#video'),
+    animateText = $('#animate'),
     text = $('.slide__text-center'),
     slideBtnStart = $('.slide__start'),
     container = $('.container'),
     skipBtn = $('.slide__skip'),
-    skipBtnPlay = $('.slide__play'),
+    slideBtnPlay = $('.slide__play'),
     slideOne = $('.slide_one'),
     slideTwo = $('.slide_two'),
     playBtn = $('.slide__play'),
@@ -16,55 +21,56 @@
     noise = document.querySelector('.noise'),
     noiseStyle = noise.style;
 
-  /*шум*/
-  getRandom = function (max) {
-    return Math.floor(Math.random() * ++max);
-  };
 
-  setInterval(function () {
-    noiseStyle.backgroundPosition = getRandom(200) + 'px ' + getRandom(200) + 'px';
-  }, 50);
+  /*анимация переходов слайдов*/
 
-  /*cursur*/
-  $(document)
-    .mousemove(function (e) {
-      cursor
-        .eq(0)
-        .css({
-          left: e.pageX,
-          top: e.pageY
-        });
-      setTimeout(function () {
-        cursor
-          .eq(1)
-          .css({
-            left: e.pageX,
-            top: e.pageY
-          });
-      }, 100);
-    });
 
-  $(document).ready(function () {
+  autoCloseVideo = function () {
     video.on('ended', function () {
       container.addClass('container_change-size');
-      $(skipBtn).removeClass('slide__skip_show');
+      skipBtn.removeClass('slide__skip_show');
       video.attr('loop', 'loop');
       video.get(0).volume = 0;
       video.get(0).currentTime = 0;
       video.trigger('play');
+      addAnimateText();
     });
-  });
+  };
+
+  addAnimateText = function () {
+    setTimeout(function () {
+      animateText.addClass('slide__animate-text_show');
+      setTimeout(function () {
+        animateText.removeClass('slide__animate-text_start');
+      }, 500);
+    }, 1000);
+  };
+
+  removeAnimateText = function () {
+    animateText.removeClass('slide__animate-text_show');
+    animateText.addClass('slide__animate-text_hide');
+    setTimeout(function () {
+      animateText.removeClass('slide__animate-text_hide');
+      animateText.addClass('slide__animate-text_start');
+    }, 500);
+  };
 
   changePosition = function () {
     slideOne.removeClass('slide_active');
     slideTwo.addClass('slide_active');
   };
+  
+  isTouchDevice = function () {
+    return 'ontouchstart' in document.documentElement;
+  }
 
   skipBtn.on("click", function () {
     video.get(0).volume = 0;
     video.trigger('play');
+    video.attr('loop', 'loop');
     container.removeClass('container_hide');
     container.addClass('container_change-size');
+    addAnimateText();
     $(this).removeClass('slide__skip_show');
     if (!slideTwo.hasClass("slide_active")) {
       changePosition();
@@ -72,10 +78,23 @@
   });
 
   playBtn.on("click", function () {
-    video.get(0).volume = 1;
-    video.get(0).currentTime = 0;
-    container.removeClass('container_change-size');
-    $(skipBtn).addClass('slide__skip_show');
+    setTimeout(function () {
+      video.removeAttr('loop');
+      video.get(0).volume = 1;
+      video.get(0).currentTime = 0;
+      container.removeClass('container_change-size');
+      skipBtn.addClass('slide__skip_show');
+    }, 100);
+    removeAnimateText();
+    autoCloseVideo();
+  });
+
+  slideBtnStart.on("click", function () {
+    video.trigger('currentTime');
+    video.trigger('play');
+    autoCloseVideo();
+    container.removeClass('container_hide');
+    changePosition();
   });
 
   if (!slideTwo.hasClass("slide_active")) {
@@ -85,16 +104,55 @@
     }, 3000);
   }
 
-  slideBtnStart.on("click", function () {
-    video.trigger('currentTime');
-    video.trigger('play');
-
-    container.removeClass('container_hide');
-    changePosition();
-  });
-
-  skipBtnPlay.hover(function () {
+  slideBtnPlay.hover(function () {
     container.toggleClass('container_hover');
   });
+
+  if (!isTouchDevice()) {
+    /*шум*/
+    getRandom = function (max) {
+      return Math.floor(Math.random() * ++max);
+    };
+    setTimeout(function () {
+      setInterval(function () {
+        noiseStyle.backgroundPosition = getRandom(200) + 'px ' + getRandom(200) + 'px';
+      }, 50);
+    }, 5000);
+
+    /*cursur*/
+    $(document)
+      .mousemove(function (e) {
+        cursor
+          .eq(0)
+          .css({
+            left: e.pageX,
+            top: e.pageY
+          });
+        setTimeout(function () {
+          cursor
+            .eq(1)
+            .css({
+              left: e.pageX,
+              top: e.pageY
+            });
+        }, 100);
+      });
+  } else {
+    $(document)
+      .mousemove(function (e) {
+        cursor
+          .eq(0)
+          .css({
+            display: 'none'
+          });
+        setTimeout(function () {
+          cursor
+            .eq(1)
+            .css({
+              display: 'none'
+            });
+        }, 100);
+      });
+  }
 
 })();
